d885480ad8fba1896752d5a4dc6de99ae40bf702
Chintan Sanghrajka <71487393+chintan-sanghrajka@users.noreply.github.com>
2023-10-08 00:00:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://chatter-frontend-ecru.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  // Allow credentials (if needed)
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
+
 const server = app.listen(PORT, () => {
   console.log("Listening on port: ", PORT);
 });
